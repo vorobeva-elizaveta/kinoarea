a7cba56820d2a510c7b5ff20c25658aa16a3232d
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-06-06 21:39:12 +0300

    added user header

---
+++ src/widgets/user/navigation/TheUserHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * компонент шапки для кабинета пользователя
+ */
+</script>
+
+<template>
+  <header class="user-header" id="user-header">
+    <div class="user-header__container">
+      <span class="user-header__logo-span">
+        <img src="" alt="kinoarea-logo" />
+      </span>
+      <ul class="user-header__menu">
+        <li class="menu__item"></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style lang="scss"></style>
