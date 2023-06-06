fc641f364def35daed32fb89f9eaba1446bc10f2
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-06-06 21:39:29 +0300

    added base user cabinet view

---
+++ src/views/user/BaseUserCabinet.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import UserHeader from '@/widgets/user/navigation/TheUserHeader.vue'
+</script>
+
+<template>
+  <div class="base-user-cabinet">
+    <div class="base-user-cabinet__container">
+      <user-header></user-header>
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
