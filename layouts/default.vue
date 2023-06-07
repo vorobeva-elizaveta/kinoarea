18492a8005ed9c1beff5567040d0d8c1a3c54d7a
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-06-07 14:22:15 +0300

    added pages

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="default-layout">
+    <div class="default-layout__container">123</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
+]
