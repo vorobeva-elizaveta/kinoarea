2744c7b8af262006063921662b0073dabbc50b81
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-06-14 18:50:07 +0300

    fixed add movie request

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import moviesCollection, {getMovies} from "../models/movies.js";
 import db from "../firebase.js";
 import { setDoc, doc, getDoc,where } from 'firebase/firestore/lite';
+import { Filter } from "firebase-admin/firestore";
 
 
 const router = Router()
@@ -12,9 +13,21 @@ router.get('/movies', async (req, res)=> {
 })
 
 router.post('/add-movie', async (req, res) => {
-    console.log(req.body.id);
-    let ref = await moviesCollection.where('id', '==', req.body.id).get()
-    res.send(ref.id)
+    let ref = await moviesCollection.where(
+        Filter.or(
+            Filter.where('id', '==', req.body.id),
+            Filter.where('kp-id', '==', req.body.id)
+        )
+    ).get()
+
+    if(ref.empty) {
+        moviesCollection.add(req.body)
+        res.send(ref.docs[0])
+        return 
+    }
+
+    res.send('already exist')
+    return 
 })
 
 export default router
