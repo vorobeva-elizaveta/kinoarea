8c68208bab94214feeea204f5e6074d72b05fe48
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-06-14 13:14:21 +0300

    added changes

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,20 @@
 <script setup>
 const {
-  data: products,
-  pending,
-  error,
-} = useFetch("https://api.kinopoisk.dev/v1.3/movie/random", {
-  headers: {
-    "X-API-KEY": "55NC2F3-4244ZY8-K8V339X-48BR2V9",
-  },
-});
+    data: products,
+    pending,
+    error,
+  } = await useFetch("https://api.kinopoisk.dev/v1.3/movie/random", {
+    headers: {
+      "X-API-KEY": "55NC2F3-4244ZY8-K8V339X-48BR2V9",
+    },
+  }),
+  { data: testData } = useFetch("/api/hello");
 </script>
 
 <template>
   <div>
     <h2>About!</h2>
+    {{ testData }}
     {{ pending }}
     {{ error }}
     <pre>{{ products }}</pre>
